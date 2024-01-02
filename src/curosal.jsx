573141f6e32fa0e59539041818c4d448cf0a8089
ayman yasser <112979502+AymanYasser242@users.noreply.github.com>
2024-01-02 20:02:04 +0200

    Update curosal.jsx

--- src/curosal.jsx
+++ src/curosal.jsx
@@ -2,7 +2,7 @@ import { Carousel } from "antd";
 
 const CarouselSlider = () => {
   return (
-    <Carousel autoplay infinite autoplaySpeed={40000} className="main-carousel">
+    <Carousel autoplay infinite autoplaySpeed={3000} className="main-carousel">
       <div>
         <h3>Give Your Interior Modern Look</h3>
       </div>
